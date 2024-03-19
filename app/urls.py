7bb32942b1961aa96155d90fe827e6b0fe70c9c5
Sebastiaan Pfennigwerth <baspenny@gmail.com>
2024-03-19 11:04:19 +0100

    Adds client detail

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app.views import index
+from app.views import client_views
+
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('client/', client_views.list, name='client_list'),
+    path('client/<id>', client_views.detail, name='client_detail'),
 ]
--- app/views/client_views.py
+++ app/views/client_views.py
@@ -1,9 +1,11 @@
+import time
+
 from django.shortcuts import render
 from app.models import Client
 from django.db.models import Q
 
 
-def index(request):
+def list(request):
     qs = Client.objects.all()
 
     if request.htmx:
@@ -18,3 +20,13 @@ def index(request):
         return render(request, 'partials/clients.html', context={'clients': qs})
 
     return render(request, 'home.html', context={'clients': Client.objects.all()})
+
+
+def detail(request, id):
+    if request.method == 'POST':
+        pass
+    if request.method == 'PUT':
+        pass
+    if request.method == 'DELETE':
+        pass
+    return render(request, 'partials/client_detail.html', context={'client': Client.objects.get(id=id)})
